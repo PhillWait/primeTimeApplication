87e6a8d92bf58f4bf2ec5b8010bb6d2b981c4d6c
Phill265 <phillip.meinert@brightstraining.com>
2024-12-23 14:56:41 +0100

    fix imports

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,6 +1,7 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
-import {NavigationSidebarComponent} from './festures/navigation-sidebar/navigation-sidebar.component';
+import {NavigationSidebarComponent} from './features/navigation-sidebar/navigation-sidebar.component';
+
 
 @Component({
   selector: 'app-root',
